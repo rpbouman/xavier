ead99716b15b3b8cf1e3c73ae2eb881c264ac0ca
rpbouman <Roland.Bouman@gmail.com>
2015-04-13 23:46:04 +0200

    Finished the tabular visualization.

--- src/js/xaviertabpane.js
+++ src/js/xaviertabpane.js
@@ -398,82 +398,55 @@ adopt(XavierWelcomeTab, XavierTab);
       return;
     }
     this.dataset = dataset;
-    var columnAxis = queryDesigner.getColumnAxis();
-    var columnAxisInfo = this.getColumnInfo(columnAxis);
-    var columns = this.getColumns(columnAxisInfo)
-    var cells = [];
 
-    var primaryAxis, secondaryAxis, measureColumns, j, m = 0, cellOrdinal, cell, onlyMeasures = false;
-    if (dataset.hasRowAxis()) {
-      primaryAxis = dataset.getRowAxis();
-      n = primaryAxis.hierarchyCount();
-      secondaryAxis = dataset.getColumnAxis();
-      var measuresIndex = columnAxis.getHierarchyIndex("Measures");
-      measureColumns = columnAxisInfo.splice(measuresIndex, 1);
-      measureColumns = measureColumns[0];
-      for (j in  measureColumns) {
-        m++;
+    var columns = queryDesigner.dataGridColumns, column,
+        cells = [], rows = [], axisMap = [], cellmap = [], i, n = columns.length
+    ;
+    for (i = 0; i < n; i ++) {
+      column = columns[i];
+      if (iDef(column.memberIndex)) {
+        axisMap[column.memberIndex] = column.dataGridColumnIndex;
       }
+      else
+      if (iDef(column.cellIndex)){
+        cellmap[column.cellIndex] = column.dataGridColumnIndex;
+      }
+    }
+
+    var axis, cellset;
+    if (dataset.hasRowAxis()){
+      axis = dataset.getRowAxis();
       cellset = dataset.getCellset();
     }
     else {
-      primaryAxis = dataset.getColumnAxis();
-      if (primaryAxis.getHierarchyNames().indexOf("Measures") === 0 && primaryAxis.hierarchyCount() === 1) {
+      axis = dataset.getColumnAxis();
+      if (queryDesigner.getColumnAxis().hasMeasures()){
         cellset = dataset.getCellset();
-        measureColumns = columnAxisInfo[0];
-        n = 0;
-        onlyMeasures = true;
-      }
-      else {
-        n = primaryAxis.hierarchyCount();
       }
     }
 
-    if (onlyMeasures) {
-      var i = 0, column, cellOrdinal, values = [];
-      for (column in measureColumns){
-        cellOrdinal = i;
-        if (cellset.cellOrdinal() === cellOrdinal) {
-          column = measureColumns[column];
-          values[column.index] = cellset.cellValue();
+    var cellOrdinal = 0;
+    axis.eachTuple(function(tuple){
+      var members = tuple.members, i, n = members.length, values = [];
+      for (i = 0; i < n; i++){
+        values[axisMap[i]] = members[i].Caption;
+      }
+      n = cellmap.length;
+      for (i = 0; i < n; i++) {
+        if (cellset.cellOrdinal() === cellOrdinal++) {
+          values[cellmap[i]] = cellset.cellValue();
           cellset.nextCell();
         }
-        i++;
       }
       cells.push(values);
-    }
-    else {
-      primaryAxis.eachTuple(function(tuple){
-        var members = tuple.members, i, member, column, cellOrdinal, values = [];
-        values.length = columns.length;
-        for (i = 0; i < n; i++){
-          member = members[i];
-          column = columnAxisInfo[i][member.LNum];
-          values[column.index] = member.Caption;
-        }
+    }, this);
 
-        i = 0;
-        for (column in measureColumns){
-          cellOrdinal = tuple.index * m + i;
-          if (cellset.cellOrdinal() === cellOrdinal) {
-            column = measureColumns[column];
-            values[column.index] = cellset.cellValue();
-            cellset.nextCell();
-          }
-          i++;
-        }
 
-        cells.push(values);
-      }, this);
-    }
-
-    var data = {
+    this.dataGrid.setData({
       columns: columns,
-      rows: [],
+      rows: rows,
       cells: cells
-    };
-
-    this.dataGrid.setData(data);
+    });
     this.dataGrid.doLayout();
   },
   doLayout: function(){
@@ -490,52 +463,27 @@ var XavierTableTab;
   arguments.callee._super.apply(this, [conf]);
 }).prototype = {
   text: gMsg("New Table"),
-  getMdx: function(cubeName){
-    var columnAxis = this.getColumnAxis();
-
-    //rewrite query if necessary
-    //if the query includes measures, then we want those on the column axis,
-    //and all others on the row axis.
-    var rowAxis;
-    if (columnAxis.hasMeasures()) {
-      var measuresIndex = columnAxis.getMeasuresIndex();
-      this.fireEvents(false);
-      this.updateDom(false);
-      //create new dummy axis
-      rowAxis = this.createAxis({id: Xmla.Dataset.AXIS_ROWS});
-      //move the measures to the new axis
-      this.moveMeasures(columnAxis, rowAxis);
-      //swap axes. Measures is now on columns, dimensions on rows.
-      this.swapAxes(columnAxis, rowAxis);
-    }
-
-    //rely on the prototypes method to actually generate MDX
-    var mdx = QueryDesigner.prototype.getMdx.call(this, cubeName);
-
-    //undo rewrite query if necessary
-    if (rowAxis) {
-      //move the measures hierarchy back and remove the new axis.
-      this.moveMeasures(rowAxis, columnAxis, measuresIndex);
-      this.swapAxes(columnAxis, rowAxis);
-      rowAxis.destroy();
-      this.fireEvents(true);
-      this.updateDom(true);
-    }
-    return mdx;
-  },
   getMdx: function(cubeName) {
-    //for each hierarchy
-    //  find the lowest level (the one with the highest LNum)
-    //  replace any higher level with a calculated measure that gets the member caption at that level
+    //datagrid columns: this is what we will be using to render the result
+    var dataGridColumns = this.dataGridColumns = [], dataGridColumn, cellIndex = 0, memberIndex = 0;
     var columnAxis = this.getColumnAxis();
     var oldSetDefs = [], hierarchies = {}, measures = [];
 
-    //
+    //analyze the query.
     columnAxis.eachSetDef(function(setDef, setDefIndex, hierarchy, hierarchyIndex){
+      //in principle, each item on the axis will become a column in the datagrid.
+      dataGridColumns.push(dataGridColumn = {
+        label: setDef.caption,
+        dataGridColumnIndex: dataGridColumns.length
+      });
+      setDef.dataGridColumn = dataGridColumn;
       oldSetDefs.push(setDef);
       var hierarchyName = columnAxis.getHierarchyName(hierarchy);
       if (columnAxis.isMeasureHierarchy(hierarchy)) {
         measures.push(setDef);
+        //measure values will apper in cells,
+        //so mark where we can find the cells for those datagrid columns
+        dataGridColumn.cellIndex = cellIndex++;
       }
       else {
         //make an inventory of the levels in the hierarchy.
@@ -564,7 +512,8 @@ var XavierTableTab;
       }
     }, this);
 
-    //
+    //now that we know what the lowest level is for each hierarchy,
+    //divide the set of items on those that are lowest in the hierarchy, and all others.
     var rowAxisItems = [], calculatedMeasures = [], hierarchyStat, setDefs, setDef, level;
     for (hierarchy in hierarchies) {
       hierarchyStat = hierarchies[hierarchy];
@@ -574,31 +523,36 @@ var XavierTableTab;
       setDef = setDefs[hierarchyStat.maxLevel];
       //push it to the row axis.
       rowAxisItems.push(setDef);
+      setDef.dataGridColumn.memberIndex = memberIndex++;
       //remove it from the column axis.
       delete setDefs[hierarchyStat.maxLevel];
 
       //create a calculated measure for the other levels
       for (level in setDefs){
         setDef = setDefs[level];
+        setDef.dataGridColumn.cellIndex = cellIndex++;
         calculatedMeasures.push(setDef);
       }
     }
 
-    //silence events and dom updates so we can rewrite the query.
-    this.fireEvents(false);
-    this.updateDom(false);
-
-    if (measures.length || calculatedMeasures.length){
+    //if we have measures, or have hierarchies with more than one level
+    //then we need to rewrite the query to 2 axes.
+    if (calculatedMeasures.length || measures.length && rowAxisItems.length){
+      //silence events and dom updates so we can rewrite the query.
+      this.fireEvents(false);
+      this.updateDom(false);
       //create new dummy axis
-      var rowAxis;
-      rowAxis = this.createAxis({id: Xmla.Dataset.AXIS_ROWS});
+      var rowAxis = this.createAxis({
+        id: Xmla.Dataset.AXIS_ROWS,
+        hasEmptyCheckBox: false
+      }),
+          i, n = calculatedMeasures.length,
+          calculatedMeasure, metadata, hierarchyName, caption, expression
+      ;
       //move the measures to the new axis
       if (columnAxis.hasMeasures()) {
         this.moveMeasures(columnAxis, rowAxis);
       }
-      var i, n = calculatedMeasures.length,
-          calculatedMeasure, metadata, hierarchyName, caption, expression
-      ;
       for (i = 0; i < n; i++) {
         calculatedMeasure = calculatedMeasures[i];
         metadata = calculatedMeasure.metadata;
@@ -618,7 +572,7 @@ var XavierTableTab;
           calculation: expression
         });
       }
-      //
+      //place the lowest level items on the column axis.
       columnAxis.clear();
       n = rowAxisItems.length;
       for (i = 0; i < n; i++) {
@@ -632,23 +586,24 @@ var XavierTableTab;
     //rely on the prototypes method to actually generate MDX
     var mdx = QueryDesigner.prototype.getMdx.call(this, cubeName);
 
-    if (measures.length || calculatedMeasures.length){
+    if (rowAxis){
 
       //restore the state of the query designer
       this.swapAxes(columnAxis, rowAxis);
-      if (rowAxis) {
-        this.removeAxis(rowAxis);
-      }
+      this.removeAxis(rowAxis);
+
+      //re-store column axis to original state.
       columnAxis.clear();
       n = oldSetDefs.length;
       for (i = 0; i < n; i++){
         setDef = oldSetDefs[i];
         columnAxis.addMember(i-1, setDef.type, setDef.metadata);
       }
-    }
 
-    this.fireEvents(true);
-    this.updateDom(true);
+      //start firing events and updating the dom again.
+      this.fireEvents(true);
+      this.updateDom(true);
+    }
 
     //
     return mdx;
